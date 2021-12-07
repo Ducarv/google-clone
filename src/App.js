64896efb57e487cd7c44f7905c05a28099bce7a3
Eduardo <educarvalho009@gmail.com>
2021-12-07 16:11:57 -0400

    search bar

--- src/App.js
+++ src/App.js
@@ -2,16 +2,17 @@ import React from 'react';
 import Home from './pages/Home';
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Search from './components/Search';
 
 function App() {
   return (
     <div className="app">
       <Router>
         <Routes>
-          <Route path="/search">
+          <Route path="/search" element={<Search/>}>
             <h1>This is the search page</h1>
           </Route>
-          <Route path="/">
+          <Route path="/" element={<Home/>}>
             <Home />
           </Route>
         </Routes>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,26 +1,38 @@
 import React from "react";
-import './Home.css';
+
+import "./Home.css";
+import googleLogo from "../images/Google_logo.svg.png";
+
 import { Link } from "react-router-dom";
+import AppsIcon from "@material-ui/icons/Apps";
+import { Avatar } from "@material-ui/core";
+import Search from "../components/Search";
 
 export default function Home() {
   return (
-      <div className="home">
-        <h1>This home page</h1>
-
-        <div className="home__header">
-          <div className="home__headerLeft">
-            <Link to='/about'>about</Link>
-          </div>
-
-          <div className="home__headerRight">
-            
-          </div>
+    <div className="home">
+      <div className="home__header">
+        <div className="home__headerLeft">
+          <Link to="/about">About</Link>
+          <Link to="/store">Store</Link>
         </div>
-
-        <div className="home__body">
-
+        <div className="home__headerRight">
+          <Link to="/gmail">Gmail</Link>
+          <Link to="/images">Images</Link>
+          <AppsIcon />
+          <Avatar />
         </div>
-
       </div>
+
+      <div className="home__body">
+        <img
+          src={googleLogo}
+          alt=""
+        />
+        <div className="home__inputContainer">
+          <Search/>
+        </div>
+      </div>
+    </div>
   );
 }
